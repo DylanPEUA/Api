50378b4f17bab8b6a114557d1504e9dc712a6f76
Dylan Peua <dylanpeua2@gmail.com>
2025-09-03 18:27:33 +0200

    Anulation de l'utilisation de mongoose et modification en utilisant seulement MongoDb pour la connexion

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,24 @@
-import mongoose from "mongoose";
+import { MongoClient } from "mongodb";
+
+let client;
+let db;
 
 export async function connectDB(uri) {
   try {
-    mongoose.set("strictQuery", true);
-    await mongoose.connect(uri);
-    console.log("MongoDB connecté");
+    client = new MongoClient(uri);
+    await client.connect();
+    db = client.db(); // nom de la base est pris dans l’URI
+    console.log(" MongoDB connecté");
+    return db;
   } catch (err) {
-    console.error("Erreur de connexion MongoDB :", err.message);
+    console.error(" Erreur de connexion MongoDB :", err.message);
     process.exit(1);
   }
+}
+
+export function getDB() {
+  if (!db) {
+    throw new Error(" La base n'est pas encore connectée !");
+  }
+  return db;
 }
